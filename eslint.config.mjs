565862fd0703844a2faa65fd6b512fc2b9a56fe4
Matias Bastarrica <205562029+MatiasBastarrica@users.noreply.github.com>
2026-02-19 10:45:26 -0300

    Initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,18 @@
+import js from "@eslint/js";
+import globals from "globals";
+import prettier from "eslint-config-prettier";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    languageOptions: {
+      globals: globals.browser,
+    },
+  },
+  js.configs.recommended,
+  prettier,
+  {
+    ignores: ["dist", "node_modules"],
+  },
+]);
