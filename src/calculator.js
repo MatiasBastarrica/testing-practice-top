8005345ffb590ea35af924c538ed23853217eb4d
MatiasBastarrica <205562029+MatiasBastarrica@users.noreply.github.com>
2026-02-19 11:42:26 -0300

    Write the calculator object and its methods

---
+++ src/calculator.js
@@ -0,0 +1,14 @@
+export const calculator = {
+  add(num1, num2) {
+    return 4 + 2;
+  },
+  subtract(num1, nu1) {
+    return 4 - 2;
+  },
+  multiply(num1, num2) {
+    return 4 * 2;
+  },
+  divide(num1, num2) {
+    return 4 / 2;
+  },
+};
