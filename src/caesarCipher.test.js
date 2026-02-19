c69e5cd75a117e9878619e01e395efcebdedfab1
MatiasBastarrica <205562029+MatiasBastarrica@users.noreply.github.com>
2026-02-19 19:46:09 -0300

    Refactor the tests for caesarCipher

--- src/caesarCipher.test.js
+++ src/caesarCipher.test.js
@@ -1,10 +1,31 @@
 import { describe, expect, test } from "@jest/globals";
 import { caesarCipher } from "./caesarCipher.js";
 
-test("Caesar cipher 'abc' with shift 3", () => {
-  expect(caesarCipher("abc", 3)).toBe("def");
-});
+test("Caesar cipher a bunch of stirngs", () => {
+  const testCases = [
+    {
+      string: "abc",
+      shift: 3,
+      expect: "def",
+    },
+    {
+      string: "xyz",
+      shift: 3,
+      expect: "abc",
+    },
+    {
+      string: "wxy",
+      shift: 3,
+      expect: "zab",
+    },
+    {
+      string: "abcde",
+      shift: 1,
+      expect: "bcdef",
+    },
+  ];
 
-test("Caesar cipher 'xyz' with shift 3", () => {
-  expect(caesarCipher("xyz", 3)).toBe("abc");
+  testCases.forEach((testCase) => {
+    expect(caesarCipher(testCase.string, testCase.shift)).toBe(testCase.expect);
+  });
 });
