761f552bec9b0136a6a50c0acce87c3c9ce9f275
MatiasBastarrica <205562029+MatiasBastarrica@users.noreply.github.com>
2026-02-19 15:21:59 -0300

    Refactor the tests for the calculator

--- src/calculator.test.js
+++ src/calculator.test.js
@@ -1,38 +1,6 @@
 import { describe, expect, test } from "@jest/globals";
 import { calculator } from "./calculator.js";
 
-test("Sum 4+2", () => {
-  expect(calculator.add(4, 2)).toBe(6);
-});
-
-test("Subtract 4-2", () => {
-  expect(calculator.subtract(4, 2)).toBe(2);
-});
-
-test("Multiply 4*2", () => {
-  expect(calculator.multiply(4, 2)).toBe(8);
-});
-
-test("Divide 4/2", () => {
-  expect(calculator.divide(4, 2)).toBe(2);
-});
-
-test("Sum 15+3", () => {
-  expect(calculator.add(15, 3)).toBe(18);
-});
-
-test("Subtract 15-3", () => {
-  expect(calculator.subtract(15, 3)).toBe(12);
-});
-
-test("Multiply 15*3", () => {
-  expect(calculator.multiply(15, 3)).toBe(45);
-});
-
-test("Divide 15/3", () => {
-  expect(calculator.divide(15, 3)).toBe(5);
-});
-
 test("Use the calculator", () => {
   const testCases = [
     {
@@ -57,18 +25,13 @@ test("Use the calculator", () => {
     },
   ];
 
+  const operations = ["add", "subtract", "multiply", "divide"];
+
   testCases.forEach((testCase) => {
-    expect(calculator.add(testCase.num1, testCase.num2)).toBe(
-      testCase.expected.add,
-    );
-    expect(calculator.subtract(testCase.num1, testCase.num2)).toBe(
-      testCase.expected.subtract,
-    );
-    expect(calculator.multiply(testCase.num1, testCase.num2)).toBe(
-      testCase.expected.multiply,
-    );
-    expect(calculator.divide(testCase.num1, testCase.num2)).toBe(
-      testCase.expected.divide,
-    );
+    operations.forEach((operation) => {
+      expect(calculator[operation](testCase.num1, testCase.num2)).toBe(
+        testCase.expected[operation],
+      );
+    });
   });
 });
