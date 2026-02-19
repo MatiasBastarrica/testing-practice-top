0c031302362d5b60c9e4fe831c1974bc017d446f
MatiasBastarrica <205562029+MatiasBastarrica@users.noreply.github.com>
2026-02-19 15:36:56 -0300

    Add a new test for caesarCipher

--- src/caesarCipher.test.js
+++ src/caesarCipher.test.js
@@ -4,3 +4,7 @@ import { caesarCipher } from "./caesarCipher.js";
 test("Caesar cipher 'abc' with shift 3", () => {
   expect(caesarCipher("abc", 3)).toBe("def");
 });
+
+test("Caesar cipher 'xyz' with shift 3", () => {
+  expect(caesarCipher("xyz", 3)).toBe("abc");
+});
