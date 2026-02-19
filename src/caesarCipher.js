5b5d5b8c28091426a82c0b07566e29b24077bd94
MatiasBastarrica <205562029+MatiasBastarrica@users.noreply.github.com>
2026-02-19 19:40:57 -0300

    Refactor caesarCipher

--- src/caesarCipher.js
+++ src/caesarCipher.js
@@ -28,9 +28,23 @@ export function caesarCipher(string, shift) {
     "z",
   ];
 
-  if (string === "abc") {
-    return "def";
-  } else if (string === "xyz") {
-    return "abc";
+  let cipherArr = [];
+  const lettersArr = string.split("");
+  lettersArr.forEach((letter) => {
+    let startIndex = alphabet.indexOf(letter);
+    let finalIndex = findFinalIndex(startIndex, shift);
+    cipherArr.push(alphabet[finalIndex]);
+  });
+
+  function findFinalIndex(startIndex, shift) {
+    let finalIndex = startIndex + shift;
+    if (finalIndex > alphabet.length - 1) {
+      let newIndex = 0;
+      let newShift = finalIndex - (alphabet.length - 1) - 1;
+      return findFinalIndex(newIndex, newShift);
+    }
+    return finalIndex;
   }
+
+  return cipherArr.join("");
 }
--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 import "./styles.css";
+import { caesarCipher } from "./caesarCipher.js";
 
 console.log("Hello, Odinite!!!");
+
+console.log(caesarCipher("xyz", 3));
