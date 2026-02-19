5f51b6cb079548fbd8644bc78e3cf2681c4a9e0a
MatiasBastarrica <205562029+MatiasBastarrica@users.noreply.github.com>
2026-02-19 12:02:39 -0300

    Refactor the calculator

--- src/calculator.js
+++ src/calculator.js
@@ -1,30 +1,14 @@
 export const calculator = {
   add(num1, num2) {
-    if (num1 === 4 && num2 === 2) {
-      return 4 + 2;
-    } else if (num1 === 15 && num2 === 3) {
-      return 15 + 3;
-    }
+    return num1 + num2;
   },
   subtract(num1, num2) {
-    if (num1 === 4 && num2 === 2) {
-      return 4 - 2;
-    } else if (num1 === 15 && num2 === 3) {
-      return 15 - 3;
-    }
+    return num1 - num2;
   },
   multiply(num1, num2) {
-    if (num1 === 4 && num2 === 2) {
-      return 4 * 2;
-    } else if (num1 === 15 && num2 === 3) {
-      return 15 * 3;
-    }
+    return num1 * num2;
   },
   divide(num1, num2) {
-    if (num1 === 4 && num2 === 2) {
-      return 4 / 2;
-    } else if (num1 === 15 && num2 === 3) {
-      return 15 / 3;
-    }
+    return num1 / num2;
   },
 };
