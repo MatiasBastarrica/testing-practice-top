73cd09941b0df8873479a35ba50a8eac29519bda
MatiasBastarrica <205562029+MatiasBastarrica@users.noreply.github.com>
2026-02-19 21:47:42 -0300

    Remove comments

--- src/analyzeArray.test.js
+++ src/analyzeArray.test.js
@@ -2,13 +2,6 @@ import { describe, expect, test } from "@jest/globals";
 import { analyzeArray } from "./analyzeArray.js";
 
 test("Analazye [1,8,3,4,2,6]", () => {
-  // expect(analyzeArray([1, 8, 3, 4, 2, 6])).toBe({
-  //   average: 4,
-  //   min: 1,
-  //   max: 8,
-  //   length: 6,
-  // });
-
   const arr = [1, 8, 3, 4, 2, 6];
 
   const actual = analyzeArray(arr);
